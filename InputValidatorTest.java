060947944bf8bcfab3f92f5a25d1a08665cbb7f9
narynbekkyzy <narynbekova16@gmail.com>
2025-04-30 19:22:07 -0400

    More failing tests

--- InputValidatorTest.java
+++ InputValidatorTest.java
@@ -20,6 +20,35 @@ public class InputValidatorTest {
         assertFalse (InputValidator.validateNameField(""));
         assertFalse (InputValidator.validateNameField(" "));
     }
+
+    @Test
+    public void testNameMinLength() {
+        assertFalse(InputValidator.validateNameField("A"));
+        assertTrue(InputValidator.validateNameField("Al"));
+    }
+
+    @Test
+    public void testNameWithSpaces() {
+        assertTrue(InputValidator.validateNameField("Nailia Narynbek"));
+    }
+
+    @Test
+    public void testNameWithDiacritics() {
+        assertTrue(InputValidator.validateNameField("Zoë"));
+        assertTrue(InputValidator.validateNameField("Björk"));
+    }
+
+    @Test
+    public void testNameWithInvalidCharacters() {
+        assertFalse(InputValidator.validateNameField("John123"));
+        assertFalse(InputValidator.validateNameField("!@#"));
+        assertFalse(InputValidator.validateNameField("Anna*"));
+    }
+
+    @Test
+    public void testNameNull() {
+        assertFalse(InputValidator.validateNameField(null));
+    }
     
     // CREATE MORE TESTS HERE
     
