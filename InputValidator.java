ec6d5d39226eb2e5e45298fefa815a0ce2e2473f
Jair Delgado <jair.delgado23@kzoo.edu>
2025-04-30 20:04:18 -0400

    Adding my code before pulling as to not lose progress

--- InputValidator.java
+++ InputValidator.java
@@ -1,10 +1,66 @@
 /*
+Author: Jair Delgado 
+Assisted By: Maddie 
  * This is the InputValidator class. Please complete this class with
  * appropiate JavaDoc comments, method and code comments, and the appropiate
  * methods to validate inputs from the user. 
  */
 
-public class InputValidator {
-    
+import java.text.BreakIterator; //found off google, input off github that helps take diacritic marks and takes letters from other languages off as well
+import java.util.Locale; //helps with character definitions 
 
-}
+public class InputValidator 
+{
+    
+    public static boolean validatelastNameField(String input) { //This is our boolean that will return true if more than 2 letters 
+        BreakIterator charIterator = BreakIterator.getCharacterInstance(Locale.getDefault()); //initialize  
+        charIterator.setText(input); // 
+
+        int letterCount = 0; 
+        int start = charIterator.first();
+        int end = charIterator.next();
+
+        while (end != BreakIterator.DONE) {
+            String Lastname = input.substring(start, end);
+            int codePoint = Lastname.codePointAt(0);
+            int type = Character.getType(codePoint);
+
+            if (type == Character.UPPERCASE_LETTER || type == Character.LOWERCASE_LETTER || // Defining what were considering as letters as to not take special characters like "@ or $"
+                type == Character.TITLECASE_LETTER || type == Character.MODIFIER_LETTER || 
+                type == Character.OTHER_LETTER) {
+
+                letterCount++; //increase letter count 
+                if (letterCount >= 2) { 
+                    return true; //if greater than 2 than accepted 
+                }
+            }
+
+            start = end;
+            end = charIterator.next();
+        }
+
+        return false;
+    }
+
+
+
+ 
+ public static boolean validatePhonenumber(String phoneNumber) // started to use regular expression (Maddie in the CC helped)
+     {
+        //The following text is more for my own understanding but is also an explanation of each character
+        //^ is the start of the string 
+        //(? is an optional open parathesis as ? is making the symbol optional, then 3 digits then closing the optional paranthesis, more optional dash, 3 more digits and 
+        //then optional last dash then 4 digits closing the string 
+
+        return phoneNumber.matches("^\\(?\\d{3}\\)?[- ]?\\d{3}[- ]?\\d{4}$"); // returns phoneNumber that contain 
+     }
+            
+public static boolean validateDescription(String Descrip) // started to use regular expression (Maddie in the CC helped)
+        {
+        //The following text is more for my own understanding but is also an explanation of each character in RegEx
+        //^ is the start of the string 
+        //"." is any character with 1 to 1000 digits then $ closes the string 
+            return Descrip.matches("^.{1,1000}$");
+        }
+            
+    }
--- InputValidatorTest.java
+++ InputValidatorTest.java
@@ -6,20 +6,33 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class InputValidatorTest {
-    
-    /*
-     * Example test method for a possible Name field validator method in your 
-     * InputValidator class.
-     * For more information about assertion tests check: 
-     * https://www.tutorialspoint.com/junit/junit_using_assertion.htm
-     */
+
+public class InputValidatorTest
+ {
     @Test
-    public void testNameFieldIsNotEmpty() {
-        // assertTrue(InputValidator.validateNameField("..."));
-        // assertFalse (InputValidator.validateNameField("..."));
-    }
     
-    // CREATE MORE TESTS HERE
+    public void testLastnameFrieldIsnotEmpty() // We are testing to ensure that the Last name has more than 2 characters and accepts Diacritic marks
+    {
+        //True cases 
+        assertTrue(InputValidator.validatelastNameField("García")); //Testing to make sure it takes Diacritic marks. 
+        assertTrue(InputValidator.validatelastNameField("'Niyah'")); //testing for names beginning with "'"
+        assertTrue(InputValidator.validatelastNameField("A Niyah")); //testing if it takes names with spaces
+        //False cases 
+        assertFalse(InputValidator.validatelastNameField("'  '")); //testing to ensure sure you cannot just input a " ' " 
+        assertFalse(InputValidator.validatelastNameField("@&$")); //Makes sure that you cannot take a special character 
+        assertFalse(InputValidator.validatelastNameField("A")); //Makes sure you have to have atleast 2 characters 
+        //True cases 
+        assertTrue(InputValidator.validatePhonenumber("(708)506-2612"));
+        assertTrue(InputValidator.validatePhonenumber("708-506-2612"));
+       //False cases 
+        assertFalse(InputValidator.validatePhonenumber("01"));   
+
+        //True cases 
+        assertTrue(InputValidator.validateDescription("01"));   
+        assertTrue(InputValidator.validateDescription("01"));   
+        assertTrue(InputValidator.validateDescription("01"));   
+        //False cases 
+        assertFalse(InputValidator.validateDescription("1"));   
+    }
     
 }
